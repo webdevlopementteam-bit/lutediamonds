93e0699a3c36f0eca06704f39e2fc8f675067173
himanshutiwariidev <himanshu.tiwarii.dev@gmail.com>
2026-08-29 15:24:10 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter_Tight } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -17,13 +18,48 @@ export const metadata = {
   ],
   icons: {
     icon: "/favicon.png"
-  }
+  },
+  verification: {
+    google: "frrnSyedko1vui8qhw-fyN1VDp9bTiHCS94p-UnNbKg",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${interTight.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W5XRDP9C');`}
+        </Script>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W5XRDP9C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HF3CCEHC68"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HF3CCEHC68');`}
+        </Script>
+
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
